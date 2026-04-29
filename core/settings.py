f304d226c20bb143918fb8dc5eb8eab40d75f83f
Soul3vans <elbert.escobar23@gmail.com>
2026-04-28 22:18:18 -0400

    fix: template dirs para React

--- core/settings.py
+++ core/settings.py
@@ -123,9 +123,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
