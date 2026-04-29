bb33efec8306d12b6d08ee167a3d7c2bd10e4c20
Soul3vans <elbert.escobar23@gmail.com>
2026-04-28 21:36:19 -0400

    fix: fallback SQLite si no hay DATABASE_URL

--- core/settings.py
+++ core/settings.py
@@ -158,6 +158,13 @@ if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Seguridad en producción
 if not DEBUG:
